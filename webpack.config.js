5ad6c4719cb3add7f827e278d7fe9158cbe169c1
HoffsMH <hecker.matthew@gmail.com>
2018-04-04 21:24:25 -0400

    read some more of this guide https://www.valentinog.com/blog/react-webpack-babel/#How_to_set_up_React_Webpack_and_Babel_setting_up_Webpack
    and added some more setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,27 @@
-// const path = require('path');
+const path = require('path');
 // const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
-  // output: {
-  //   filename: 'bundle.js',
-  //   path: path.resolve(__dirname, 'dist'),
-  // },
-  // plugins: [
-  //   new HtmlWebpackPlugin({
-  //     hash: true,
-  //     template: 'index.html',
-  //   }),
-  // ],
+  entry: ['./src/js/index.js'],
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      hash: true,
+      template: './src/index.html',
+    }),
+  ],
 };
